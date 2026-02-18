3d7e26400d3ea294cf66c74895e9f775d2dd184c
limjt <wjdxor0405@gmail.com>
2026-02-18 19:57:15 +0900

    Jaksadata.h분리

--- GameData/GameData.h
+++ GameData/GameData.h
@@ -36,19 +36,6 @@ public:
   std::string name;
 };
 
-class Jaksadata{
-public:
-    GameUserData* gameuserdata;
-    std::vector<int> sonTils;
-    int lastTile = -1;//쯔모 혹은 론 한 마지막 패
-    Berims* berims;
-    int isRich = 0;//0: 리치아님, 1: 일반리치, 2; 더블리치
-    Baram baram = Dong;
-    std::vector<HuroBlcck*> hurolist;
-    std::vector<AnkanBlcok*> ankanList;
-    std::vector<int> nukilist;
-};
-
 class GameData{
 public:
   int nowTurnJaksaIndex = 0;//현재 누구 차례인지
---
+++ GameData/Jaksadata.h
@@ -0,0 +1,14 @@
+class GameUserData;
+
+class Jaksadata{
+public:
+    GameUserData* gameuserdata;
+    std::vector<int> sonTils;
+    int lastTile = -1;//쯔모 혹은 론 한 마지막 패
+    Berims* berims;
+    int isRich = 0;//0: 리치아님, 1: 일반리치, 2; 더블리치
+    Baram baram = Dong;
+    std::vector<HuroBlcck*> hurolist;
+    std::vector<AnkanBlcok*> ankanList;
+    std::vector<int> nukilist;
+};
